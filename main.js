4171664353392c3b576197b067a744b039011f90
SarahSchool27 <143737919+SarahSchool27@users.noreply.github.com>
2026-04-02 20:37:24 -0400

    Updated Parameters

--- main.js
+++ main.js
@@ -14,29 +14,9 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
   const quadVertexShader = gulls.constants.vertex
 
 
-  // our fragment shader, header contains bindings
-  /*const fragmentShader = `
-   @group(0) @binding(0) var<uniform> res : vec2f;
-   @group(0) @binding(1) var<uniform> frame : f32;
-   @group(0) @binding(2) var<uniform> slider : f32;
-    @group(0) @binding(3) var backSampler: sampler;
-    @group(0) @binding(4) var backBuffer: texture_2d<f32>;
-    @group(0) @binding(5) var<uniform> mouse : vec3f;
+  // our fragment shader
+  //const fragmentShader =  await gulls.import('./shaders/header.wgsl') + await gulls.import( './shaders/default.wgsl' ) //doesn't work do to caching problems
 
-    // NOTE THAT THERE IS A DIFFERENT GROUP NUMBER FOR THE
-    // VIDEO TEXTURE BELOW. This lets gulls easily rebind
-    // the texture for each frame, without having to rebind
-    // the other variables in group 0. Given the new group,
-    // the binding index resets to 0.
-    @group(1) @binding(0) var videoBuffer:    texture_external;
-
-    @fragment 
-    fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
-      return vec4f( mouse.x, mouse.y, mouse.z, 1. );
-    
-    
-      }`*/
-  //const fragmentShader =  await gulls.import('./shaders/header.wgsl') + await gulls.import( './shaders/default.wgsl' )
   const fragmentShader = 
     `
     @group(0) @binding(0) var<uniform> res : vec2f;
@@ -48,6 +28,11 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
     //user uniforms
     @group(0) @binding(5) var<uniform> slider_rippleDens : f32;
     @group(0) @binding(6) var<uniform> user_color : vec3f;
+    @group(0) @binding(7) var<uniform> do_backgroundWave_large : f32;
+    @group(0) @binding(8) var<uniform> do_backgroundWave_small : f32;
+    @group(0) @binding(9) var<uniform> grid_mode : f32;
+    @group(0) @binding(10) var<uniform> main_dens : f32;
+    @group(0) @binding(11) var<uniform> main_height : f32;
 
     // NOTE THAT THERE IS A DIFFERENT GROUP NUMBER FOR THE
     // VIDEO TEXTURE BELOW. This lets gulls easily rebind
@@ -73,14 +58,14 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
       //line2
   		var line_p2 = p;
       line_p2.y -= .5;
-      var height_modifier =  0.5* cos(frame/200);
-      line_p2.y += height_modifier*sin(5*line_p2.x + frame/100) + (line_p1.y*2);
+      var height_modifier =  0.5* cos(frame/200) * main_height;
+      line_p2.y += height_modifier*sin(main_dens*line_p2.x + frame/100) + (line_p1.y*2);
       var line_2 = pow(abs(.0025/line_p2.y ),3.);
 	  	color -= vec3(line_2);
 
       var line_p3 =p;
       line_p3.y -= .5;
-      line_p3.y +=  1.2*height_modifier*sin(5*line_p2.x + frame/100) + (line_p1.y*2);
+      line_p3.y +=  1.2*height_modifier*sin(main_dens*line_p2.x + frame/100) + (line_p1.y*2);
       var line_3 = pow(abs(.0025/line_p3.y ),3.);
 	  	color -= vec3(line_3);
 
@@ -99,8 +84,8 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
       var st =p;
       st += noise(st*20)*sin(frame/1000);
 
-      let large_noise_mask = smoothstep( 0.01, 0.5, noise(((p+line_p1)+frame/8000)*5));
-      let small_noise_mask = smoothstep(0.01,0.5,noise((st +line_p1)*10));
+      let large_noise_mask = smoothstep( 0.01, 0.5, noise(((p+line_p1*do_backgroundWave_large)+frame/8000)*5));
+      let small_noise_mask = smoothstep(0.01,0.5,noise((st +line_p1*do_backgroundWave_small)*10));
       let noise_mask =  vec3(clamp(3 * min(large_noise_mask, small_noise_mask),0,1));
 
       //get video
@@ -110,10 +95,30 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
       color -= video_mask;
       color = max( video_mask * video.rgb * user_color, color);
 
-      color -= 1 - vec3(rippleGrid(p, slider_rippleDens, 10));
+
+      let horizantal_mask = step(0.5, p.y);
+
+      if (grid_mode == 0 ){ //default
+        color -= 1 - vec3(rippleGrid(p, slider_rippleDens, 10, false));
+      } else if(grid_mode == 1){ //mirrored
+        color -= 1 - (vec3(rippleGrid(p, slider_rippleDens, 10, false))*(horizantal_mask)
+        + vec3(rippleGrid(p, slider_rippleDens, 10, true))*(1-horizantal_mask));
+      } else if(grid_mode == 2){//top_only
+        color -= 1 - (vec3(1.0)*(horizantal_mask)
+        + vec3(rippleGrid(p, slider_rippleDens, 10, true))*(1-horizantal_mask));
+      
+      } else if(grid_mode == 3){ //bottom only
+        color -= 1 - (vec3(rippleGrid(p, slider_rippleDens, 10, false))*(horizantal_mask)
+        + vec3(1)*(1-horizantal_mask));
+      }else if(grid_mode == 4){//overlap
+        color -= 1 - vec3(rippleGrid(p, slider_rippleDens, 10, false));
+        color -= 1 - vec3(rippleGrid(p, slider_rippleDens, 10, true));
+      }
+      
 
       color += vec3(node_mask);
 
+      //color = vec3(horizantal_mask);
 
       
 
@@ -122,12 +127,17 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
 
     } 
 
-    fn rippleGrid(p : vec2f, ripple_dens : f32, ripple_height : f32) -> vec3f{
+    fn rippleGrid(p : vec2f, ripple_dens : f32, ripple_height : f32, doOffset : bool) -> vec3f{
       let PI = 3.14159;
 
       //grid code
       var mod_p = p;
-      mod_p.y += sin(mod_p.x*ripple_dens)/ripple_height;
+
+      if(doOffset == true){ 
+        mod_p.y += sin(mod_p.x*ripple_dens +PI)/ripple_height;
+      }else if (doOffset == false){
+        mod_p.y += sin(mod_p.x*ripple_dens)/ripple_height;
+      }
       mod_p.x += frame/1000;
       var grid_pos = grid(mod_p, 20.0, 40.0); //multiplication for grid
       var color : vec3f = vec3f(0.0);
@@ -211,8 +221,17 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
 
   //control scheme (USER Uniforms) ------------
   const PARAMS = {
-  rippleDens: 10,
-  tint_color: {r: 0.29, g: 0.39, b: 0.27},
+    rippleDens: 10,
+    grid_mode: 0.0,
+    
+    main_dens : 5.0,
+    main_height : 1.0,
+    
+    tint_color: {r: 0.29, g: 0.39, b: 0.27},
+    do_backgroundWave_large : true,
+    do_backgroundWave_small :true,
+    do_invert_background_movement : false
+
   };
 
 
@@ -220,8 +239,12 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
 
   //tweakplane uniforms
   let u_tp_sl_rippleDens = sg.uniform(PARAMS.rippleDens)
-
   let u_tp_v3_color = sg.uniform(Object.values(PARAMS.tint_color))
+  let u_do_backgroundWave_large = sg.uniform(PARAMS.do_backgroundWave_large);
+  let u_do_backgroundWave_small = sg.uniform(PARAMS.do_backgroundWave_small);
+  let u_grid_mode = sg.uniform(PARAMS.grid_mode);
+  let u_mainDens = sg.uniform(PARAMS.main_dens);
+  let u_main_height = sg.uniform(PARAMS.main_height);
 
   //tweakplane bindings
   pane.addBinding(PARAMS, 'rippleDens',  {min:0, max:50})  
@@ -234,12 +257,73 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
       console.log('(last)');
     }*/
   });
+
+  pane.addBinding(PARAMS,'grid_mode', {
+    options:{
+        default : 0.0,
+        mirrored : 1.0,
+        top_only : 2.0,
+        bottom_only : 3.0,
+        overlap : 4.0
+    },
+    })
+    .on('change', (ev) => {
+     u_grid_mode.value = PARAMS.grid_mode;
+     //console.log(Object.values(PARAMS.tint_color));
+  });
+
+  pane.addBinding(PARAMS, 'main_dens',  {min:1, max:12})  
+  .on('change', (ev) => {
+    
+    //console.log(ev.value.toFixed(2));
+     u_mainDens.value = ev.value.toFixed(2);
+    //tell if last input 
+    /*if (ev.last) {
+      console.log('(last)');
+    }*/
+  });
+
+
+  pane.addBinding(PARAMS, 'main_height',  {min:-3, max:3})  
+  .on('change', (ev) => {
+    
+    //console.log(ev.value.toFixed(2));
+     u_main_height.value = ev.value.toFixed(2);
+    //tell if last input 
+    /*if (ev.last) {
+      console.log('(last)');
+    }*/
+  });
   
   pane.addBinding(PARAMS, 'tint_color', {color: {type: 'float'}})
    .on('change', (ev) => {
      u_tp_v3_color.value = Object.values(PARAMS.tint_color);
      //console.log(Object.values(PARAMS.tint_color));
   });
+
+  pane.addBinding(PARAMS, 'do_backgroundWave_large')
+    .on('change', (ev)=> {
+      if(PARAMS.do_backgroundWave_large == true){
+        u_do_backgroundWave_large.value =  1.0;
+      }else{
+        u_do_backgroundWave_large.value =  0.0;
+      }
+    });
+  
+
+  pane.addBinding(PARAMS, 'do_backgroundWave_small')
+    .on('change', (ev)=> {
+      if(PARAMS.do_backgroundWave_small == true){
+        u_do_backgroundWave_small.value =  1.0;
+      }else{
+        u_do_backgroundWave_small.value =  0.0;
+      }
+      
+
+    });
+
+  
+  
 
 
   //end control scheme (USER Uniforms) ------------
@@ -279,6 +363,11 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
       //user uniforms
       u_tp_sl_rippleDens,
       u_tp_v3_color,
+      u_do_backgroundWave_large,
+      u_do_backgroundWave_small,
+      u_grid_mode,
+      u_mainDens,
+      u_main_height,
 
 
       sg.video( Video.element )
